599ae33470f3963603cd7b873746176993873e69
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-02-04 14:02:11 -0600

    Add age and speed validation functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function oldEnough(age) {
+  if (age == 18) {
+    return "old enough";
+  }
+}
+function underAge(age) {
+  if (underage == 16) {
+    return "underage";
+  }
+}
+function legalspeed(speed) {
+  if (speed == 49) {
+    return "legal speed";
+  }
+}
+function isGoldenYears(age) {
+  if (age == 66) {
+    return "golden years";
+  }
+}
+      
+  
 
